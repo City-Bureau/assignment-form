e31119e1fa626717468d0d875d7a1d00d59540fa
Jim Benton <jim@autonomousmachine.com>
2017-11-30 20:59:27 -0600

    Project bootstrap.

---
+++ src/components/AssignmentForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="form">
+    <h1>Documenters Assignment Application</h1>
+    <div class="loading" v-if="loading">
+      Loading...
+    </div>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssignmentForm',
+  data () {
+    return {
+      selected: [],
+      loading: false,
+      events: [],
+      documenters: [],
+      error: null,
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      const getDocumenters = fetch('http://localhost:8080/api/documenters').then((response) => {
+      });
+
+      const getEvents = fetch('http://localhost:8080/api/events').then((response) => {
+      });
+      Promise.all(getEvents, getDocumenters).then(() => { this.loading = false });
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
